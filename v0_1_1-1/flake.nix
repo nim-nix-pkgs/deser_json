9e99f1d4c04e53d8579d7b10f2789e8db6de9343
hugosenari <hugosenari@gmail.com>
2022-08-26 21:43:52 -0300

    chore: re index deser_json

--- v0_1_1-1/flake.nix
+++ v0_1_1-1/flake.nix
@@ -7,19 +7,11 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-deser_json-master.flake = false;
-  inputs.src-deser_json-master.ref   = "refs/heads/master";
-  inputs.src-deser_json-master.owner = "gabbhack";
-  inputs.src-deser_json-master.repo  = "deser_json";
-  inputs.src-deser_json-master.type  = "github";
-  
-  inputs."faststreams".owner = "nim-nix-pkgs";
-  inputs."faststreams".ref   = "master";
-  inputs."faststreams".repo  = "faststreams";
-  inputs."faststreams".dir   = "master";
-  inputs."faststreams".type  = "github";
-  inputs."faststreams".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."faststreams".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs.src-deser_json-v0_1_1-1.flake = false;
+  inputs.src-deser_json-v0_1_1-1.ref   = "refs/tags/v0.1.1-1";
+  inputs.src-deser_json-v0_1_1-1.owner = "gabbhack";
+  inputs.src-deser_json-v0_1_1-1.repo  = "deser_json";
+  inputs.src-deser_json-v0_1_1-1.type  = "github";
   
   inputs."deser".owner = "nim-nix-pkgs";
   inputs."deser".ref   = "master";
@@ -29,16 +21,24 @@
   inputs."deser".inputs.nixpkgs.follows = "nixpkgs";
   inputs."deser".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."jsony".owner = "nim-nix-pkgs";
+  inputs."jsony".ref   = "master";
+  inputs."jsony".repo  = "jsony";
+  inputs."jsony".dir   = "1_1_3";
+  inputs."jsony".type  = "github";
+  inputs."jsony".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jsony".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-deser_json-master"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-deser_json-v0_1_1-1"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-deser_json-master";
+    src  = deps."src-deser_json-v0_1_1-1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
